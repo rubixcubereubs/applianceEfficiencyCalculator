84bbd19afced69f3627bb710a55d8e94929c9801
rubixcubereubs <programmingreubs@gmail.com>
2020-09-29 06:43:58 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Calculator from "./calculator";
+import Footer from "./footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
