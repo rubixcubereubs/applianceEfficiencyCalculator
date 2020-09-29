3c82f8f672a614e3ed71142eb66b77f6df416836
rubixcubereubs <programmingreubs@gmail.com>
2020-09-29 07:57:21 +0100

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header(props) {
   return (
     <div className="header">
-      <h3 className="logo">Energy Efficiency Calculator</h3>
+      <h3 className="logo">Appliance Efficiency Calculator</h3>
       <p className="result">
         Result: The {props.data.appliance} will use approximately £
         {props.data.totalAmount} per month
